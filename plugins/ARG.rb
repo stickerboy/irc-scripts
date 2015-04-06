ec2ad152eed5512b23fa929c90442816c80c1549
Sepheus <sepheus@sepheus.co.uk>
2015-04-06 01:37:12 +0100

    ARG commands ported across sans munge.

--- plugins/ARG.rb
+++ plugins/ARG.rb
@@ -13,14 +13,16 @@ class ARG
 	match /ask .+\?$/i, method: :ask
 	match /sitrep/i, method: :sitrep
 	match /potato/i, method: :potato
+	match /arg .+/i, method: :arg
 
 	def load_db(m)
 		#Probably want to figure out a way of loading this just once.
 		@responses = YAML.load_file("#{DB_FOLDER}/ask.yaml")
+		@arg = YAML.load_file("#{DB_FOLDER}/arg.yaml")
 	end
 
 	def ask(m)
-		m.reply "#{@responses[rand(0...@responses.length)]}"
+		m.reply "#{@responses[rand(0..@responses.length)]}"
 	end
 
 	def sitrep(m)
@@ -29,6 +31,11 @@ class ARG
 
 	def potato(m)
 		m.reply "Hi, how are you holding up? Because I'm a potato - #{POTATO_URL}"
+	end
+
+	def arg(m)
+		reply = @arg[m.message.downcase!.split(" ")[1]]
+		m.reply reply.nil? ? @arg["help"].first : reply.first
 	end
 
 end
