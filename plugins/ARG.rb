f37b6e0a00870e30df6ff6e55067e1cfab192b00
Sepheus <sepheus@sepheus.co.uk>
2015-05-09 16:14:41 +0100

    Remove HTS Changes stuff.

--- plugins/ARG.rb
+++ plugins/ARG.rb
@@ -34,7 +34,6 @@ class ARG
 	listen_to :join, method: :join_events
 	
 	timer 180, method: :timer
-	timer 600, method: :hts_changes
 
 	match /ask .+\?$/i, method: :ask
 	match /sitrep/i, method: :sitrep
@@ -61,8 +60,6 @@ class ARG
 		@arg = YAML.load_file("#{config[:db]}/arg.yaml")
 		@slaps = YAML.load_file("#{config[:db]}/slaps.yaml")
 		@dates = YAML.load_file("#{config[:db]}/dates.yaml")
-
-		@htsSum = page_sum(HUNT_THE_SIGNAL_URL)
 	end
 
 	def load_rss(m)
@@ -141,16 +138,6 @@ class ARG
 
 	end
 
-	def hts_changes
-		currentSum = page_sum(HUNT_THE_SIGNAL_URL)
-		bot.info "Expected MD5: #{@htsSum}"
-		bot.info "Current MD5: #{currentSum}"
-		if @htsSum != currentSum
-			Channel('#Halo5').notice "http://huntthesignal.com has changed!"
-			@htsSum = currentSum
-		end
-	end
-
 	def join_events(m)
 		notify_mib(m)
 	end
@@ -187,12 +174,6 @@ class ARG
 
 	def identify(m)
 		bot.irc.send("ns identify #{config[:password]}")
-	end
-
-	def page_sum(page)
-		response = Net::HTTP.get_response(URI(page))
-		data = open(response['location']).read
-		Digest::MD5.hexdigest(data)
 	end
 
 	def crickets(m)
